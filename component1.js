f92a76dfbdb2492ace63cae9d49f7bbc75afdb97
VarshaPalepu19 <69585002+VarshaPalepu19@users.noreply.github.com>
2022-05-06 18:26:28 +0530

    Add files via upload

---
+++ component1.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import assist_ss from '@salesforce/resourceUrl/screenshot1';
+import assist_ss1 from '@salesforce/resourceUrl/screenshot2';
+
+export default class Component1 extends LightningElement {
+    sfimage=assist_ss;
+    sfimage1=assist_ss1;
+}
